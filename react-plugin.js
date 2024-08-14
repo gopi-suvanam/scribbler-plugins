2184e6a03603d4d5ed8323b470a9522762352d36
Gopi Krishna Suvanam <68820170+gopi-suvanam@users.noreply.github.com>
2024-08-14 12:30:03 +0530

    Create react-plugin.js

---
+++ react-plugin.js
@@ -0,0 +1,25 @@
+await Promise.all([
+    import('https://unpkg.com/react@17/umd/react.production.min.js'),
+    import('https://unpkg.com/react-dom@17/umd/react-dom.production.min.js'),
+  ]);
+if (typeof Babel === 'undefined') {
+  	// Babel is not defined, load it
+	await import("https://unpkg.com/@babel/standalone/babel.min.js");
+}
+
+
+const temp_evaluate= worker.evaluate;
+const new_evaluate=code=>{
+  const regex = /^\/\/>\s*jsx/i;
+
+ if( regex.test(code))
+ {
+	 const updatedCode = code.replace(regex, "");
+     const transpiledCode = Babel.transform(updatedCode, { presets: ['react'] }).code;
+	 return  temp_evaluate(	transpiledCode);
+	}
+	return temp_evaluate(code);
+	
+}
+
+worker.evaluate=new_evaluate;
