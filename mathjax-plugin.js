1ee4443a8860745349578c0d46ce0490f21402d2
Gopi Krishna Suvanam <68820170+gopi-suvanam@users.noreply.github.com>
2024-08-14 14:13:03 +0530

    Update mathjax-plugin.js

--- mathjax-plugin.js
+++ mathjax-plugin.js
@@ -1,20 +1,8 @@
 await import("https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js");
 
-const outputDivs = document.querySelectorAll('.output');
-let typesetting = false;
-
-const observer = new MutationObserver(async () => {
-  if (!typesetting) {
-    typesetting = true;
-    await MathJax.typesetPromise();
-    console.log('Typesetting complete');
-    typesetting = false;
-  }
-});
-
-outputDivs.forEach((div) => {
-  observer.observe(div, {
-    childList: true,
-    subtree: true,
-  });
-});
+const old_run=worker.run;
+const new_run=async function(_block_id){
+   old_run(_block_id); 
+	await MathJax.typesetPromise();
+}
+worker.run=new_run;
